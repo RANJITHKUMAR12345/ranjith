3c0b9947c5c3779f84c737268aebd21eebddb3b2
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-20 15:42:47 +0530

    Create beginset16.c

---
+++ beginset16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+
+{
+    int n;
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+        printf("is leap year");
+    }
+    else
+    {
+        printf("is not leap year");
+    }
+}
