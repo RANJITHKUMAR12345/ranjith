5e592c8532f224e0cf836f9112585d61f7bbb78c
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-20 16:03:54 +0530

    Create beginset23.c

---
+++ beginset23.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,i,count=0;
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+count++;
+}
+if(count==0)
+printf("prime number");
+else
+printf("not a prime no");
+}
