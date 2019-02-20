1bda97c8535d54c72d31447204029ca77e2e6dc0
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-20 16:19:59 +0530

    Update beginset27.c

--- beginset27.c
+++ beginset27.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 void main()
 {
-    int num,v,result,digit;
+    int n,v,result,digit;
     printf("enter the num value");
-    scanf("%d",&num);
-    v=num;
+    scanf("%d",&n);
+    v=n;
     while(v!=0);
     {
         digit=v%10;
         result+=digit*digit*digit;
         v=v/10;
     }
-    if(result==num);
+    if(result==n);
     printf("amstrong number");
 }
 else
