17c40e4777c563afcb1cc1ecf4c0e098db92a6f7
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-26 16:15:43 +0530

    Create beginset37.c

---
+++ beginset37.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char s[10],i;
+printf("enter a string");
+scanf("%s",&s);
+{
+if((s[i]=='a')||(s[i]=='z'))
+{
+printf("no numeric");
+
+}
+else
+{
+    printf("yes");
+}
+}
