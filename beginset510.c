28a17a7eb782cfc938d77e670e516b400daa419a
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-26 21:48:27 +0530

    Create beginset510.c

---
+++ beginset510.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+	int n,flag=0,temp;
+	scanf("%d",&n);
+	temp=n;
+	while(temp!=1)
+	{
+		if(temp%2!=0)
+		{
+			flag=1;
+			break;
+		}
+		temp=temp/2;
+	}
+	if(flag==0)
+	{
+		printf("yes");
+	}
+	else if(flag==1)
+	{
+		printf("no");
+	}
+	}
