3ba8f6536d8403e88a2c567ddfd87a92b81fda19
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-26 19:57:58 +0530

    Create beginset42.c

---
+++ beginset42.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int b=1,i;
+for(i=0;a[i]<=20;i++)
+{
+scanf("%c",&a[i]);
+if((a[i]==' ')&&((a[i+1]>='a')||(a[i+1]<='z')))
+b=b+1;
+}
+printf("%d",b);
+}
