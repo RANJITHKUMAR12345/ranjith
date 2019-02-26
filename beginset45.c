11f9bb73d1d2e3da034902b5c4ce21134a9ce235
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-02-26 21:02:38 +0530

    Create beginset45.c

---
+++ beginset45.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[100];
+	int b=0,i,h;
+	scanf("%s",&a);
+	h=strlen(a);
+	for(i=0;i<=h;i++)
+	{
+	if((a[i]>='0')&&(a[i]<='9'))
+		b++;
+	}
+	printf("%d",b);
+}
