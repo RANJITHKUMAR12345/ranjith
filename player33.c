c4b8251860b80f95bb42d858a50397c7b1d30d5c
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-03-25 16:22:57 +0530

    Create player33.c

---
+++ player33.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int a[10],k,i=0,x,n,j,max=0;
+    scanf("%d %d",&n,&x);
+    for(i=0;i<n;i++)
+    {
+	    scanf("%d",&a[i]);
+	}
+	j=0;
+    while(j<x)
+	{
+	    scanf("%d",&k);
+	    a[n]=k;
+	    n++;
+	   for(i=0;i<n;i++)
+	    {
+	        if(a[i]>=max)
+	        {
+	            max=a[i];
+	        }
+	    }
+	    printf("%d ",max); 
+	j++; 
+	}
+	return 0;
+  
+  
+}
