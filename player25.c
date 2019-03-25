6918a7ed339b520e33064596c97a0c983d45fbc8
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-03-25 16:05:12 +0530

    Create player25.c

---
+++ player25.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define maxi 255
+#include<conio.h>
+ 
+int main()
+{
+    char str[100];
+    int freq[maxi];
+    int i = 0, max,j,ascii;
+    printf("Enter any string: ");
+    gets(str);
+    for(i=0;i<maxi;i++)
+    {
+        freq[i] = 0;
+    }
+    i=0;
+    while(str[i]!='\0')
+    {
+        ascii = (int)str[i];
+        freq[ascii] += 1;
+        i++;
+    }
+    max = 0;
+    for(i=0;i<maxi;i++)
+    {
+        if(freq[i]>freq[max])
+            max=i;
+    }
+    printf("Maximum occurring character is '%c' = %d times.", max, freq[max]);
+    getch();
+}
