678bc4e5443b580740dbb008948589fd5a8a614d
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-03-25 15:25:57 +0530

    Create player11.c

---
+++ player11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	char a[1000],b[1000];
+	int start,end,count=0;
+	printf("Enter the string");
+	scanf("%s",&a);
+	while(a[count]!=0)
+	count++;
+	end=count-1;
+	for(start=0;start<count;start++)
+	{
+		b[start]=a[end];
+		end--;
+	}
+	printf("%s",b);
+}
