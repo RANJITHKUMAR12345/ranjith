b0023eb5490a963e84474aee71392ef872287599
RANJITHKUMAR12345 <36396701+RANJITHKUMAR12345@users.noreply.github.com>
2019-03-26 15:48:56 +0530

    Create player36.c

---
+++ player36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+	int i,j=0;
+	string a[i],b[j];
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]!=' ')
+		{
+			
+		b[j]=a[i];
+		j++;
+		}
+printf("%s",b);
+}
+
+
+
+}
